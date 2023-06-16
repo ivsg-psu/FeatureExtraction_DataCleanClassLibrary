6983f09e7fe7d29cedf5a84072952f8cb87232ba
aneeshbatchu <abb6486@psu.edu>
2023-06-16 12:22:52 -0400

    FEAT: Updated the functions to load data from  the sensors.

--- Functions/fcn_DataClean_loadRawData/fcn_DataClean_loadRawDataFromFile_adis_IMU_data.m
+++ Functions/fcn_DataClean_loadRawData/fcn_DataClean_loadRawDataFromFile_adis_IMU_data.m
@@ -1,4 +1,4 @@
-function adis_IMU_data = fcn_DataClean_loadRawDataFromFile_adis_IMU_data(table,rawdata,flag_do_debug)
+function adis_IMU_data = fcn_DataClean_loadRawDataFromFile_adis_IMU_data(file_path,datatype,table,flag_do_debug)
 
 % This function is used to load the raw data collected with the Penn State Mapping Van.
 % This is the GPS_Novatel data
@@ -25,59 +25,36 @@ function adis_IMU_data = fcn_DataClean_loadRawDataFromFile_adis_IMU_data(table,r
 
 %%
 
-if strcmp(data_source,'mat_file')
+if contains(file_path,'imu/data')
     
-    % IMU_Novatel.GPS_Time           = data_structure.GPS_Time;  % This is the GPS time, UTC, as reported by the unit
-    % IMU_Novatel.Trigger_Time       = data_structure.Trigger_Time;  % This is the Trigger time, UTC, as calculated by sample
-    % IMU_Novatel.ROS_Time           = data_structure.ROS_Time;  % This is the ROS time that the data arrived into the bag
-    % IMU_Novatel.centiSeconds       = data_structure.centiSeconds;  % This is the hundreth of a second measurement of sample period (for example, 20 Hz = 5 centiseconds)
-    % IMU_Novatel.Npoints            = data_structure.Npoints;  % This is the number of data points in the array
-    % IMU_Novatel.IMUStatus          = data_structure.IMUStatus;  
-    % IMU_Novatel.XAccel             = data_structure.XAccel; 
-    % IMU_Novatel.XAccel_Sigma       = data_structure.XAccel_Sigma; 
-    % IMU_Novatel.YAccel             = data_structure.YAccel; 
-    % IMU_Novatel.YAccel_Sigma       = data_structure.YAccel_Sigma; 
-    % IMU_Novatel.ZAccel             = data_structure.ZAccel; 
-    % IMU_Novatel.ZAccel_Sigma       = data_structure.ZAccel_Sigma; 
-    % IMU_Novatel.XGyro              = data_structure.XGyro; 
-    % IMU_Novatel.XGyro_Sigma        = data_structure.XGyro_Sigma; 
-    % IMU_Novatel.YGyro              = data_structure.YGyro; 
-    % IMU_Novatel.YGyro_Sigma        = data_structure.YGyro_Sigma; 
-    % IMU_Novatel.ZGyro              = data_structure.ZGyro; 
-    % IMU_Novatel.ZGyro_Sigma        = data_structure.ZGyro_Sigma; 
-    % % Event functions
-    % IMU_Novatel.EventFunctions = {}; % These are the functions to determine if something went wrong
-
-    adis_IMU_data = fcn_DataClean_initializeDataByType(datatype);
-    
-    secs = table.secs;
-    nsecs = table.nsecs;
-    adis_IMU_data.GPS_Time           = secs + nsecs*10^-9;  % This is the GPS time, UTC, as reported by the unit
-    % adis_IMU_data.Trigger_Time       = default_value;  % This is the Trigger time, UTC, as calculated by sample
-    adis_IMU_data.ROS_Time           = table.rosbagTimestamp;  % This is the ROS time that the data arrived into the bag
-    % adis_IMU_data.centiSeconds       = default_value;  % This is the hundreth of a second measurement of sample period (for example, 20 Hz = 5 centiseconds)
-    % adis_IMU_data.Npoints            = default_value;  % This is the number of data points in the array
-    % adis_IMU_data.IMUStatus          = default_value;  
-    adis_IMU_data.XAccel             = table.x_2; 
-    % adis_IMU_data.XAccel_Sigma       = default_value; 
-    adis_IMU_data.YAccel             = table.y_2; 
-    % adis_IMU_data.YAccel_Sigma       = default_value; 
-    adis_IMU_data.ZAccel             = table.z_2; 
-    % adis_IMU_data.ZAccel_Sigma       = default_value; 
-    adis_IMU_data.XGyro              = table.x_1; 
-    % adis_IMU_data.XGyro_Sigma        = default_value; 
-    adis_IMU_data.YGyro              = table.y_1; 
-    % adis_IMU_data.YGyro_Sigma        = default_value; 
-    adis_IMU_data.ZGyro              = table.z_1; 
-    % adis_IMU_data.ZGyro_Sigma        = default_value; 
-    rawdata.adis_IMU_data = adis_IMU_data;
-
+   adis_IMU_data = fcn_DataClean_initializeDataByType(datatype);
+            
+   secs = table.secs;
+   nsecs = table.nsecs;
+   adis_IMU_data.GPS_Time           = secs + nsecs*10^-9;  % This is the GPS time, UTC, as reported by the unit
+   % adis_IMU_data.Trigger_Time       = default_value;  % This is the Trigger time, UTC, as calculated by sample
+   adis_IMU_data.ROS_Time           = table.rosbagTimestamp;  % This is the ROS time that the data arrived into the bag
+   % adis_IMU_data.centiSeconds       = default_value;  % This is the hundreth of a second measurement of sample period (for example, 20 Hz = 5 centiseconds)
+   % adis_IMU_data.Npoints            = default_value;  % This is the number of data points in the array
+   % adis_IMU_data.IMUStatus          = default_value;
+   adis_IMU_data.XAccel             = table.x_2;
+   % adis_IMU_data.XAccel_Sigma       = default_value;
+   adis_IMU_data.YAccel             = table.y_2;
+   % adis_IMU_data.YAccel_Sigma       = default_value;
+   adis_IMU_data.ZAccel             = table.z_2;
+   % adis_IMU_data.ZAccel_Sigma       = default_value;
+   adis_IMU_data.XGyro              = table.x_1;
+   % adis_IMU_data.XGyro_Sigma        = default_value;
+   adis_IMU_data.YGyro              = table.y_1;
+   % adis_IMU_data.YGyro_Sigma        = default_value;
+   adis_IMU_data.ZGyro              = table.z_1;
+   % adis_IMU_data.ZGyro_Sigma        = default_value;
 
 else
     error('Please indicate the data source')
 end
 
-clear data_structure %clear temp variable
+% clear data_structure %clear temp variable
 
 
 % Close out the loading process
--- Functions/fcn_DataClean_loadRawData/fcn_DataClean_loadRawDataFromFile_adis_IMU_filtered_rpy.m
+++ Functions/fcn_DataClean_loadRawData/fcn_DataClean_loadRawDataFromFile_adis_IMU_filtered_rpy.m
@@ -1,4 +1,4 @@
-function adis_IMU_filtered_rpy = fcn_DataClean_loadRawDataFromFile_adis_IMU_filtered_rpy(table,rawdata,flag_do_debug)
+function adis_IMU_filtered_rpy = fcn_DataClean_loadRawDataFromFile_adis_IMU_filtered_rpy(file_path,datatype,table,flag_do_debug)
 
 % This function is used to load the raw data collected with the Penn State Mapping Van.
 % This is the GPS_Novatel data
@@ -25,28 +25,7 @@ function adis_IMU_filtered_rpy = fcn_DataClean_loadRawDataFromFile_adis_IMU_filt
 
 %%
 
-if strcmp(data_source,'mat_file')
-    
-    % IMU_Novatel.GPS_Time           = data_structure.GPS_Time;  % This is the GPS time, UTC, as reported by the unit
-    % IMU_Novatel.Trigger_Time       = data_structure.Trigger_Time;  % This is the Trigger time, UTC, as calculated by sample
-    % IMU_Novatel.ROS_Time           = data_structure.ROS_Time;  % This is the ROS time that the data arrived into the bag
-    % IMU_Novatel.centiSeconds       = data_structure.centiSeconds;  % This is the hundreth of a second measurement of sample period (for example, 20 Hz = 5 centiseconds)
-    % IMU_Novatel.Npoints            = data_structure.Npoints;  % This is the number of data points in the array
-    % IMU_Novatel.IMUStatus          = data_structure.IMUStatus;  
-    % IMU_Novatel.XAccel             = data_structure.XAccel; 
-    % IMU_Novatel.XAccel_Sigma       = data_structure.XAccel_Sigma; 
-    % IMU_Novatel.YAccel             = data_structure.YAccel; 
-    % IMU_Novatel.YAccel_Sigma       = data_structure.YAccel_Sigma; 
-    % IMU_Novatel.ZAccel             = data_structure.ZAccel; 
-    % IMU_Novatel.ZAccel_Sigma       = data_structure.ZAccel_Sigma; 
-    % IMU_Novatel.XGyro              = data_structure.XGyro; 
-    % IMU_Novatel.XGyro_Sigma        = data_structure.XGyro_Sigma; 
-    % IMU_Novatel.YGyro              = data_structure.YGyro; 
-    % IMU_Novatel.YGyro_Sigma        = data_structure.YGyro_Sigma; 
-    % IMU_Novatel.ZGyro              = data_structure.ZGyro; 
-    % IMU_Novatel.ZGyro_Sigma        = data_structure.ZGyro_Sigma; 
-    % % Event functions
-    % IMU_Novatel.EventFunctions = {}; % These are the functions to determine if something went wrong
+if contains(file_path,'imu/rpy/filtered')
 
     adis_IMU_filtered_rpy = fcn_DataClean_initializeDataByType(datatype);
     secs = table.secs;
@@ -71,13 +50,11 @@ if strcmp(data_source,'mat_file')
     % adis_IMU_filtered_rpy.ZGyro              = default_value; 
     % adis_IMU_filtered_rpy.ZGyro_Sigma        = default_value; 
 
-    rawdata.adis_IMU_filtered_rpy = adis_IMU_filtered_rpy;
-
 else
     error('Please indicate the data source')
 end
 
-clear data_structure %clear temp variable
+% clear data_structure %clear temp variable
 
 
 % Close out the loading process
