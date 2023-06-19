932c4d56eb47795f5862c425577757fe6597ddab
Mariam Abdellatief <97060004+Mariam-Abdellatief@users.noreply.github.com>
2023-06-19 15:36:15 -0400

    FEAT: Rename fcn_DataClean_mergeSensors and add function definition. Started the test script for the function.

---
+++ Functions/script_test_fcn_DataClean_mergeSensors.m
@@ -0,0 +1,12 @@
+% script_test_fcn_DataClean_mergeSensors.m
+% tests fcn_DataClean_mergeSensors.m
+
+% Revision history
+% 2023_06_19 - 
+% -- 
+%% Set up the workspace
+clc
+clear
+close all 
+
+%% 
